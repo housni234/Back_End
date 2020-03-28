d2851a4c59b60a2aaa653d2722238a286c596123
rahaf89 <rahaflh.900@hotmail.com>
2020-03-28 17:51:52 +0100

    Sec

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const express = require("express");
-const app = express();
-const connection = mysql.createConnection({
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '',
 	database : 'nodelogin'
 });
 
+var app = express();
 app.use(session({
 	secret: 'secret',
 	resave: true,
@@ -17,15 +22,6 @@ app.use(bodyParser.json());
 
 app.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname + '/login.html'));
-});
-
-app.get('/home', function(request, response) {
-	if (request.session.loggedin) {
-		response.send('Welcome back, ' + request.session.username + '!');
-	} else {
-		response.send('Please login to view this page!');
-	}
-	response.end();
 });
 
 app.post('/auth', function(request, response) {
@@ -48,7 +44,13 @@ app.post('/auth', function(request, response) {
 	}
 });
 
-
-app.listen(3000, function() {
-    console.log("Server is listening on port 3000. Ready to accept requests!");
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
 });
+
+app.listen(3000);
